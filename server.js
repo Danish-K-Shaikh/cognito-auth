4f6dccaf334657d043c831b8802a888b0a61c692
Navin Kadam <navinkadam1432@gmail.com>
2021-05-08 16:24:15 +0530

    app middleware added

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3003;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.status(200).json({ msg: 'Welcome to bug predators' });
+});
+
+app.post('/', function (req, res) {
+  res.status(200).json({ msg: 'Welcome to bug predators', payload: req.body });
 });
 
 app.listen(PORT, function (params) {
