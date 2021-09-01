b531e3c4a8a868e6a1dd6247928558edc165e04f
navin.kadam <navin.kadam@speedboxapp.com>
2021-09-01 14:28:01 +0530

    refresh token added

--- app/controller/app/index.js
+++ app/controller/app/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const authenticationService = require('../../services/authenticate');
 const userService = require('../../services/user');
+const { getErrorPayload } = require('../../utils/errorUtil');
 
 router.post('/login', async function (req, res) {
   try {
@@ -12,11 +13,29 @@ router.post('/login', async function (req, res) {
       posts: user.posts,
     });
 
-    res.json({ access_token: token });
+    const refreshToken = authenticationService.generateRefreshToken({
+      username,
+      posts: user.posts,
+    });
+
+    res.json({ access_token: token, refresh_token: refreshToken });
   } catch (e) {
-    const status = e.status || 500;
-    const msg = e.msg || 'Something went wrong';
-    res.status(status).json({ msg, error: e });
+    const { status, ...rest } = getErrorPayload(e, 401);
+    res.status(status).json({ ...rest });
+  }
+});
+
+router.post('/refresh-token', async function (req, res) {
+  try {
+    const refreshToken = req.headers['token'];
+    const result = await authenticationService.validateRefreshToken(refreshToken);
+    delete result.iat;
+    delete result.exp;
+    const access_token = await authenticationService.generateToken(result);
+    res.status(201).json({ access_token });
+  } catch (error) {
+    const { status, ...rest } = getErrorPayload(error, 401);
+    res.status(status).json({ ...rest });
   }
 });
 
--- app/services/authenticate/index.js
+++ app/services/authenticate/index.js
@@ -1,16 +1,27 @@
 var jwt = require('jsonwebtoken');
-const { promisyCallback } = require('../../utils/promiseUtil');
+const { promisifyCallback } = require('../../utils/promiseUtil');
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const TOKEN_EXPIRY = process.env.TOKEN_EXPIRY;
 
 function generateToken(user) {
   return jwt.sign(user, ACCESS_TOKEN, { expiresIn: TOKEN_EXPIRY });
 }
 
+function generateRefreshToken(user) {
+  return jwt.sign(user, REFRESH_TOKEN, { expiresIn: '1 days' });
+}
+
 async function validateToken(token) {
-  const user = await promisyCallback(jwt.verify, token, ACCESS_TOKEN);
+  const user = await promisifyCallback(jwt.verify, token, ACCESS_TOKEN);
   return user;
 }
 
+async function validateRefreshToken(token) {
+  return await promisifyCallback(jwt.verify, token, REFRESH_TOKEN);
+}
+
 exports.generateToken = generateToken;
+exports.generateRefreshToken = generateRefreshToken;
 exports.validateToken = validateToken;
+exports.validateRefreshToken = validateRefreshToken;
