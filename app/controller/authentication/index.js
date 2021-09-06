acd0b610c424a1335325c8d064e84d3619d2c24c
Danish Shaikh <danish.shaikh@speedboxapp.com>
2021-09-06 08:50:42 +0530

    log removed

--- app/controller/authentication/index.js
+++ app/controller/authentication/index.js
@@ -2,7 +2,6 @@ var authService = require('../../services/authenticate');
 
 module.exports = async function (req, res, next) {
   try {
-    console.log({ headers: req.headers });
     const tokenHeader = req.headers['authorization'].split(' ');
     const token = tokenHeader && tokenHeader[1];
     if (!token) return res.sendStatus(401);
